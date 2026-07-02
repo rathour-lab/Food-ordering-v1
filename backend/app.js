c41e120d1a266e3be8587454d5fd2dbd0f223ed2
Karan <jassarathour72@gmail.com>
2026-07-02 11:10:19 +0530

    menu DB

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+const express=require("express");
+const app=express();
+app.use(express.json())
+
+require('dotenv').config();
+const foodCardDB =require('./config/db')
+foodCardDB()
+
+const foodMenuSchema=require('./models/foodModel')
+
+const route=require('./routes/foodRoutes');
+app.use(route)
+
+app.listen(3000,()=>{
+console.log('server Start');
+
+});
