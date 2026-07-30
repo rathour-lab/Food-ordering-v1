94f3a92679ca6b6704a1fad76d124e4ac5f2f653
Karan <jassarathour72@gmail.com>
2026-07-30 13:07:13 +0530

    fix : port

--- backend/app.js
+++ backend/app.js
@@ -41,7 +41,7 @@ wss.on('connection',(ws)=>{
     });
 });
 })
-const PORT = process.env.PROT || 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT,()=>{
 console.log('server Start');
 
